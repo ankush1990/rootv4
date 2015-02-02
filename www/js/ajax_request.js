fb2cd1af4bff758730a303dae8961c4e57dff6b6
ankush1990 <ank_ankush@yahoo.co.in>
2015-02-02 18:27:13 +0530

    Root
    Project

---
+++ www/js/ajax_request.js
@@ -0,0 +1,35 @@
+// JavaScript Document
+
+function ajax_request(bool,pagename,data_parameters,functionname){
+	var data_url = "http://192.168.1.102:1837/"+pagename;
+	var http_request = new XMLHttpRequest();
+	try{
+	   http_request = new XMLHttpRequest();
+	}catch (e){
+	   try{
+		  http_request = new ActiveXObject("Msxml2.XMLHTTP");
+	   }catch (e) {
+		  try{
+			 http_request = new ActiveXObject("Microsoft.XMLHTTP");
+		  }catch (e){
+			 // Something went wrong
+			 alert("Your browser broke!");
+			 return false;
+		  }
+	   }
+	}
+	http_request.onreadystatechange  = function(){
+		if (http_request.readyState == 4  )
+		{
+			   var x = http_request.responseText;
+			   functionname(false,x);
+			 
+		}
+	}
+	http_request.open("POST",data_url,true);	
+	http_request.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+	http_request.send(data_parameters);
+}
+
+
+
